b46fd077e5c6745b3538c791e895191f91367b7b
maddycha <maddycha@gmail.com>
2024-01-10 18:44:25 -0500

    text-edit controls

--- js/script.js
+++ js/script.js
@@ -360,6 +360,7 @@ document.addEventListener('click', function (b) {
 
     function dragElement(elmnt) {
 
+
       var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
       if (document.getElementById(elmnt.id + "nav")) {
         // if present, the header is where you move the DIV from:
